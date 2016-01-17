6857e3d6447e961354d14c7e4b40cb4b5cc8b2a3
local hacklab <hacklab@karsina.hacklab.fi>
2016-01-17 23:05:41 +0200

    script to rebase from upstream master

---
+++ project/update_upstream.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+git checkout master
+git fetch upstream
+git rebase upstream/master master
+git checkout hhl_changes
+git rebase master
+source venv/bin/activate
+pip install -r requirements/production.txt
+./manage.py migrate
+npm run build
+./manage.py collectstatic --noinput
+for app in locale */locale; do (cd $(dirname $app) && ../manage.py compilemessages ); done
+
