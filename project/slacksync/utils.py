86b50a5e045fbfbf25b575df7313a5fc2c617e88
Eero af Heurlin <eero.afheurlin@iki.fi>
2017-05-30 01:08:41 +0300

    Working on member sync

--- project/slacksync/utils.py
+++ project/slacksync/utils.py
@@ -1,9 +1,11 @@
+# -*- coding: utf-8 -*-
 from django.conf import settings
 from slacker import Slacker
 
 
 def api_configured():
-    return bool(settings.SLACK_APIKEY)
+    return bool(settings.SLACK_APIKEY) and bool(settings.SLACK_API_USERNAME)
+
 
 def get_client(**kwargs):
     if not api_configured():
